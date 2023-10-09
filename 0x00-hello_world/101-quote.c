8f623cefb02966a9af4ec6ade6afee9a6c7bfbdf
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-10-09 14:23:18 -0700

    the standard error library

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd>
+
+/*
+ * main - Entry point
+ *
+ * Return: (1) Success
+ */
+
+int main(void)
+{
+	const char *message="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	return (1);
+}
