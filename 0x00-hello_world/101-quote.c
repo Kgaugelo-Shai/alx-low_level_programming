08b9df1d6958dcf9963b281eb6f09f579156497e
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-10-09 14:47:56 -0700

    The standard error - write command

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-	const char *msg = "and that piece of art is useful\" - Dora Korpar,	2015-10-19\n";
+	const char *m = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, msg, 59);
+	write(2, m, 59);
 	return (1);
 }
