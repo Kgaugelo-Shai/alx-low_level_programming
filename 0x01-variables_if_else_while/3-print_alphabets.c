506ee540e51ffccbf2e95dc96136e4779fe7ddd6
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-10-09 18:05:50 -0700

    lowercase uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c = 'a';
+	char C = 'A';
+
+	while ((c <= 'z' && C <= 'Z'))
+	{
+		putchar(c);
+		c++;
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+	return (0);
+}
