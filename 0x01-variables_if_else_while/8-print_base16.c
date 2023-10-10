ae5da965a70a94076f1be5a5ccd8dab2fd52cedd
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-10-09 19:08:40 -0700

    Print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i = 0;
+	char c = 'a';
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	while (c < 'f')
+	{
+		putchar(c);
+		j++;
+	}
+
+	putchar('\n');
+	return (0);
+}
