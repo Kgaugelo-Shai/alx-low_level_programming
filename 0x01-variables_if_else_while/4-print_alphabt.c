9ca5c97ef7cde4a08569f0d6295f21092877aac5
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-10-09 18:16:56 -0700

    print alphabet ii

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if ((c != q) && (c != e))
+			putchar(c);
+			c++;
+		else
+			c++;
+	}
+	putchar('\n');
+	return (0);
+}
