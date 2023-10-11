ff9dc9b5c30204689ac0754ae2530a8033ceea4a
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-10-11 13:28:14 -0700

    every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,2 +1,58 @@
 #include "main.h"
 
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int h1 = 0;
+	int h2 = 0;
+	int m1 = 0;
+	int m2 = 0;
+	int count = 1;
+
+	while (count <= 1440)
+	{
+		if (h1 <= 2)
+		{
+			_putchar(h1 + '0');
+		}
+		if (h2 <= 9)
+		{
+			_putchar(h2 + '0');
+		}
+		else
+		{
+			h2 = 0;
+			_putchar(h2 + '0');
+			h1++;
+		}
+		_putchar(':');
+		if (m1 <= 9)
+		{
+			_putchar(m1 + '0');
+		}
+		else
+		{
+			m1 = 0;
+			_putchar(m1 + '0');
+			h2++;
+		}
+		if (m2 <= 9)
+		{
+			_putchar(m2 + '0');
+			m2++;
+		}
+		else
+		{
+			m2 = 0;
+			_putchar(m2 + '0');
+			m2++;
+			m1++;
+		}
+		_putchar('\n');
+		count++;
+	}
+}
