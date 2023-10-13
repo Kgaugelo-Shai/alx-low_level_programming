e6352c286dd3c4379d34d5cf65c043428cd30d80
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-10-13 09:12:02 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - initiates Fixx-Buzz test
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int count = 1;
+
+	while (count <= 100)
+	{
+		if ((count % 3 == 0) && (count % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (count % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (count % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", count);
+		}
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
+
