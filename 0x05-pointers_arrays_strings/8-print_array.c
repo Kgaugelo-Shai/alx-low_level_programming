eb934c3c939f5b82be4a45118ec367352fb75070
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-10-17 13:14:38 -0700

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,11 +10,16 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+	int i = 0;
 
 	while (i < n)
 	{
-		printf("%d, ", a[i]);
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
 		i++;
 	}
+	printf("\n");
 }
