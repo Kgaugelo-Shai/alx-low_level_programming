cbe6cc77549b19e2b332d78cb4217124f9880498
Kgaugelo Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-10-24 19:12:23 +0200

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,26 +9,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int count = 0;
-    int i, j, found = 0;
+	unsigned int count = 0;
+	int i, j, found = 0;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        found = 0;
-        for (j = 0; accept[j] != '\0'; j++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-            if (s[i] == accept[j])
-	    {
-                count++;
-                found = 1;
-                break;
-            }
-        }
-        if (found == 0)
-	{
-            break;
-        }
-    }
+		found = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				found = 1;
+				break;
+			}
+		}
+		if (found == 0)
+		{
+			break;
+		}
+	}
 
-    return count;
-}    
+	return (count);
+}
