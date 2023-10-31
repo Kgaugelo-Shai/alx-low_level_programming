2283784271f40658300ef35022df8475c9f425c3
Kgaugelo Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-10-31 18:40:56 +0200

    main.h file

---
+++ 0-create_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars and initializes
+ * it with a specific char
+ * @size: the size of the array
+ * @c: the character the array is initialized with
+ *
+ * Return: Null if size = 0, or a pointer to array if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+
--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,44 +2,22 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: string to be converted
+ * @a: the string to be considered
  *
- * Return: the int converted from the string
+ * Return: integer converted from string
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int d = 0;
-	int n = 0;
+	int index = 0;
 	int len = 0;
-	int f = 0;
+	int num = 0;
 	int digit = 0;
 
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+	while (s[index] != '\0')
 	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
+		len++;
 	}
 
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
+	while (index < len && found == 0)
+	{
 
