d15fb9c3ff0aa3e96388effd17733c9ce6b80fd4
Kgaugelo Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-10-31 19:26:32 +0200

    Task 0

--- 0-create_array.c
+++ 0-create_array.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * create_array - creates an array of chars and initializes
@@ -10,4 +12,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *str;
+	unsigned int i;
 
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+
+	return (str);
+}
