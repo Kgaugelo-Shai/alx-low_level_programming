c7e6dc8d7c6ea03a0b79c9c786d2cdd905791b9a
Kgaugelo Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-11-08 01:30:09 +0200

    Task 4 - write a function that create s a new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+int len_str(char *str);
+char *cpy_str(char *cpy_str, char *or_str);
 
 #endif
