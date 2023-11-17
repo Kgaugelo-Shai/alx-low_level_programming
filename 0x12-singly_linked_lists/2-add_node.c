03e8f38e193294d9153ccbf6d5c69140c792217c
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-11-17 17:52:46 +0200

    Task 2: add new node to front of list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node to the beginning of a list_t list
+ * @head: the starting node of the list
+ * @str: the character string that is to be added
+ *
+ * Return: the pointer to the linked list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int i, str_len = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	if (str == NULL)
+		return (NULL);
+	new->str = strup(str);
+
+	for (i = 0; str[i] != '\0'; i++)
+		str_len++;
+
+	new->len = str_len;
+
+	if (*head != NULL)
+	{
+		new->next = *head;
+		*head = new;
+	}
+
+	return (*head);
+}
