256a36eda7994c4801f99046974a41d1f3ef5084
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-11-17 19:17:30 +0200

    Task 4: add node at the end of the list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the list_t list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	int i;
+	list_t *buff;
+
+	if (head == NULL)
+		return;
+	else
+	{
+		for (i = 0; head->next != NULL; i++)
+		{
+			buff = head->next;
+			free(head);
+			free(head->str);
+			head = buff->next;
+		}
+	}
+}
