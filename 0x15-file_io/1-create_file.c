c084446247a465c2638aeb128bfb5c2ec50fd0c0
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-11-27 19:54:59 +0200

    Task 1: create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file_dp;
+	ssize_t char_wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_dp = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
+	if (file_dp == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		char_wr = write(1, text_content, strlen(text_content));
+		if (char_wr == -1)
+		{
+			close(file_dp);
+			return (-1);
+		}
+	}
+	else
+		return (-1);
+
+	close(file_dp);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
