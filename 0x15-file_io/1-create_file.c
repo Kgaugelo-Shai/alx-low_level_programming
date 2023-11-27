91978d5f16f83b1d2ce8239b44bf2488ff3043da
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-11-27 20:11:40 +0200

    Task 1: fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,16 +21,13 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content != NULL)
+		text_content = "";
+	char_wr = write(1, text_content, strlen(text_content));
+	if (char_wr == -1)
 	{
-		char_wr = write(1, text_content, strlen(text_content));
-		if (char_wr == -1)
-		{
-			close(file_dp);
-			return (-1);
-		}
-	}
-	else
+		close(file_dp);
 		return (-1);
+	}
 
 	close(file_dp);
 	return (1);
