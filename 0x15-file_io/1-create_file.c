d7f1da37ae8ebcdc894b84c27febdf603c40a88e
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-11-27 20:00:18 +0200

    Task 1: create a file1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_dp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file_dp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
 	if (file_dp == -1)
 		return (-1);
 
