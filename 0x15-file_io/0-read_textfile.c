f96d4f62a1c8fd85769d5d5d8b450b808c9e01ac
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-08-15 01:49:26 +0200

    task 0 fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,51 +1,47 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints to the POSIX stdout.
- * @filename: name of the read file
- * @letters: number of letters that should be printed
- * Return: number of letters it could read and print
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: text file to be read
+ * @letters: where letters is the number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t numCharsRead, numCharsWrite;
-	int file_dep;
+	ssize_t char_read, char_write;
+	int file_dp;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-
-	file_dep = open(filename, O_RDONLY);
-
-	if (file_dep == NULL)
+	file_dp = open(filename, O_RDONLY);
+	if (file_dp == -1)
 		return (0);
-
-	buffer = (char *)malloc(sizeof(char) * letters);
-
+	buffer = malloc(sizeof(char) * (letters));
 	if (buffer == NULL)
 	{
-		fclose(file_dep);
+		close(file_dp);
 		return (0);
 	}
+	char_read = read(file_dp, buffer, letters);
 
-	numCharsRead = read(file_dep, buffer, letters);
-
-	if (numCharsRead == -1)
+	if (char_read == -1)
 	{
 		free(buffer);
-		fclose(file_dep);
+		close(file_dp);
 		return (0);
 	}
 
-	numCharsWrote = write(STDOUT_FILENO, buffer, numCharsRead);
-	if (numCharsWrote == -1)
+	char_write = write(STDOUT_FILENO, buffer, char_read);
+	if (char_write == -1)
 	{
-		free(buffee);
-		close(file_dep);
+		free(buffer);
+		close(file_dp);
 		return (0);
 	}
 
 	free(buffer);
-	close(file_dep);
-	return (numCharsWrote);
+	close(file_dp);
+	return (char_read);
 }
