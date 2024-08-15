99d7c795a11984ec785e404c175562ebea0f6746
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-08-15 02:51:47 +0200

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,21 +12,21 @@ int create_file(const char *filename, char *text_content)
 	int file;
 	int numCharsWR;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
-
-	numCharsWR = write(file, text_content, strlen(text_content));
-	if (numCharsWR == -1)
+	if (text_content != NULL)
 	{
-		close(file);
-		return (-1);
+		numCharsWR = write(file, text_content, strlen(text_content));
+		if (numCharsWR == -1)
+		{
+			close(file);
+			return (-1);
+		}
 	}
-
-	if (close(file) == -1)
-		return (-1);
+	close(file);
 	return (1);
 }
