0039d053648f60dcf4836212e26c352893c58eea
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2024-08-15 02:34:27 +0200

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create-file - creates a file
+ * @filename: name of the file to create
+ * @text_content: string to write to file
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_des;
+	int numCharsWR;
+
+	if (filename == NULL || text_context == NULL)
+		return (-1);
+
+	file = open(filename, O_CREATE | O_RDRW | O_TRUNC, 0600);
+	if (file_des == -1)
+		return (-1);
+
+	numCharsWR = write(file, text_content, strlen(text_content));
+	if (numChars == -1)
+		return (-1);
+
+	close(file);
+	return (1);
+}
