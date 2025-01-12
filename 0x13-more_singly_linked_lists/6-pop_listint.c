b798b7e097a058be0bb26404b38feb99a0cf97c5
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2025-01-12 15:39:05 +0200

    remove head of linked list fix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,21 +3,21 @@
 /**
  * pop_listint - removes the head of a listint_t list
  * @head: the head of the linked list
- * 
+ *
  * Return: the int value of the head of the linked list
  */
 
 int pop_listint(listint_t **head)
 {
-    listint_t *temp;
-    int n = 0;
+	listint_t *temp;
+	int n = 0;
 
-    if (*head == NULL)
-        return (n);
+	if (*head == NULL)
+		return (n);
 
-    n = (*head)->n;
-    temp = (*head)->next;
-    free(*head);
-    (*head) = temp;
-    return (n);
+	n = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	(*head) = temp;
+	return (n);
 }
